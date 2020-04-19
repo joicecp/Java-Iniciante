01596a79394aaea45b15b7d254b0060c9572e4b0
joice.pepe <joice.pepe@neomind.com.br>
2020-04-19 13:53:48 -0300

    desenvolvimento do curso de java para iniciantes

--- PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -2,11 +2,16 @@
 package primeiroprograma;
 
 
-public class PrimeiroPrograma {
+public class PrimeiroPrograma 
+{
 
-
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) //psvm (abreviação) || A PRESENÇA DO VOID indica que é um método
+    {
+        //System.out.print("Olá, mundo!!!");
+        System.out.println("Olá, mundo!"); //sout (abreviação)
+       //System = sistema 
+       //out = saída 
+       //println = imprima ou mostre
     }
     
 }
